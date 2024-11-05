1e166961ba2af6ea890e106e94d5c1f10b3687e4
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2024-11-04 18:14:47 -0600

    adding pages and building out more

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 import ijson
+from pages.home import Home
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = 'Content-Type'
@@ -10,7 +12,7 @@ app.config["CORS_HEADERS"] = 'Content-Type'
 @app.route("/")
 @cross_origin()
 def home():
-    return "Home"
+    return Home()
 
 @app.route("/spells/<spell_name>")
 @cross_origin()
@@ -24,6 +26,16 @@ def get_spell(spell_name):
         else:
             return "Spell Not Found", 404
 
+@app.route("/spells/")
+@cross_origin()
+def spells():
+    with open("spells.json", "r", encoding="utf8") as f:
+        spells = ijson.items(f, "item")
+        spellList = []
+        for i in spells:
+            spellList.append(i['name'])
+        return spellList
+    
 @app.route("/classes/<class_name>")
 @cross_origin()
 def get_class(class_name):
@@ -34,6 +46,18 @@ def get_class(class_name):
             for n in i.keys():
                 if n.lower() == class_name.lower():
                     return i[n]
+
+@app.route("/classes/")
+@cross_origin()
+def classes_home():
+    with open("classes.json", "r", encoding="utf8") as f:
+        classes = ijson.items(f, "item")
+        classlist = []
+        for i in classes:
+            for n in i.keys():
+                classlist.append(n)
+        return classlist
+                
 @app.route("/races/<race_name>")
 @cross_origin()
 def get_race(race_name):
@@ -45,7 +69,18 @@ def get_race(race_name):
                 if n.lower() == race_name.lower():
                     return i[n], 200
             else:
-                return "Pick Dwarf, Elf, Hafling, Human, DragonBorn, Gnome, Half-Elf, Half-Orc, or Tiefling"
+                return "Pick Dwarf, Elf, Haflfing, Human, DragonBorn, Gnome, Half-Elf, Half-Orc, or Tiefling"
+
+@app.route("/races/")
+@cross_origin()
+def race_home():
+    raceList = []
+    with open('races.json', "r", encoding="utf8") as f:
+        races = ijson.items(f, "item")
+        for i in races:
+            for n in i:
+                raceList.append(n)
+    return raceList
 
             
 
---
+++ pages/home.py
@@ -0,0 +1,2 @@
+def Home():
+    return "This is the Home Page of the DND api, in the URL use '/classes/className', '/spells/spellName', or '/races/raceName' to pull up information."
